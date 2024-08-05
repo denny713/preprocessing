dc33766ab0e1e93735b87dcee65521ac4110440d
Denny Afrizal <denny.afrizal@aleph-labs.com>
2024-08-05 12:16:39 +0700

    Wikipedia Scrapping

---
+++ scrap/wiki.py
@@ -0,0 +1,24 @@
+import json
+
+import wikipedia
+
+
+def scrap_wiki(keyword):
+    data = []
+    jsdata = wiki_search(keyword)
+    for cj in range(len(jsdata)):
+        item = {"title": jsdata[cj], "abstract": wiki_sum(jsdata[cj])}
+        data.append(item)
+    print(json.dumps(data))
+
+
+def wiki_search(keyword, rs=20):
+    return wikipedia.search(keyword, results=rs)
+
+
+def wiki_sum(keyword):
+    return wikipedia.summary(keyword, auto_suggest=False)
+
+
+if __name__ == '__main__':
+    scrap_wiki("spring boot")
